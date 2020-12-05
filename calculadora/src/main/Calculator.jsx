7c695463cfdd1ee32f9176b8464e1b8ab10f4338
juvenalbruno <bruno.andrade189@hotmail.com>
2020-12-05 18:52:41 -0300

    05/12

---
+++ calculadora/src/main/Calculator.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import Button from '../components/Button';
+import Display from '../components/Display';
+
+import './Calculator.css';
+
+export default class Calculator extends Component {
+    
+    constructor(props) {
+        super(props)
+        this.clearMemory = this.clearMemory.bind(this)
+        this.setOperation = this.setOperation.bind(this)
+        this.addDigit = this.addDigit.bind(this)
+    }
+
+    clearMemory() {
+        console.log('Limpar')
+    }
+
+    setOperation(operation) {
+        console.log(operation)
+    }
+
+    addDigit(n) {
+        console.log(n)
+    }
+    
+    render() {
+        return (
+            <div className="calculator">
+                <Display value={100} />
+                <Button label="AC" click={this.clearMemory} triple/>
+                <Button label="/" click={this.setOperation} operation />
+                <Button label="7" click={this.addDigit} />
+                <Button label="8" click={this.addDigit} />
+                <Button label="9" click={this.addDigit} />
+                <Button label="*" click={this.setOperation} operation/>
+                <Button label="4" click={this.addDigit} />
+                <Button label="5" click={this.addDigit} />
+                <Button label="6" click={this.addDigit} />
+                <Button label="-" click={this.setOperation} operation/>
+                <Button label="1" click={this.addDigit} />
+                <Button label="2" click={this.addDigit} />
+                <Button label="3" click={this.addDigit} />
+                <Button label="+" click={this.setOperation} operation/>
+                <Button label="0" click={this.addDigit} double/>
+                <Button label="." click={this.addDigit} />
+                <Button label="=" click={this.setOperation} operation/>
+            </div>
+        );
+    }
+}
